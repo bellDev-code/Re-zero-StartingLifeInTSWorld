2abda4b196089e5f8d9cd07c47957e9ff242d971
bellDev-code <sumaoo20@naver.com>
2022-02-28 09:38:26 +0900

    코드정리

--- solve.ts
+++ solve.ts
@@ -5,17 +5,15 @@
 function solution(grade: string) {
     console.log(`당신의 회원등급을 입력해주세요 :${grade}`)
 
+    const permission = "읽기"
+    const permission2 = "쓰기"
+    const permission3 = "삭제"
+
      if(grade === "1") {
-         const permission = "읽기"
          console.log(`${permission}권한 부여`)
      } else if (grade === "2") {
-         const permission = "읽기"
-         const permission2 = "쓰기"
          console.log(`${permission}권한 부여\n${permission2}권한 부여`)
-     } else if (grade === "3") {
-         const permission = "읽기"
-         const permission2 = "쓰기"
-         const permission3 = "삭제"
+     } else if (grade === "3") {  
          console.log(`${permission}권한 부여 \n${permission2}권한 부여\n${permission3}권한 부여`)
      } else {
          console.log("잘못된 회원등급입니다.")
