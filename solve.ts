367d8dcf7f3501e724dc7507245f82c81bd17c38
bellDev-code <sumaoo20@naver.com>
2022-03-02 17:53:41 +0900

    백준 알고리즘 문제

--- solve.ts
+++ solve.ts
@@ -2,22 +2,43 @@
 // 
 
 
-function solution(grade: string) {
-    console.log(`당신의 회원등급을 입력해주세요 :${grade}`)
+// function solution(grade: string) {
+//     console.log(`당신의 회원등급을 입력해주세요 :${grade}`)
 
-    const permission = "읽기"
-    const permission2 = "쓰기"
-    const permission3 = "삭제"
+//     const permission = "읽기"
+//     const permission2 = "쓰기"
+//     const permission3 = "삭제"
 
-     if(grade === "1") {
-         console.log(`${permission}권한 부여`)
-     } else if (grade === "2") {
-         console.log(`${permission}권한 부여\n${permission2}권한 부여`)
-     } else if (grade === "3") {  
-         console.log(`${permission}권한 부여 \n${permission2}권한 부여\n${permission3}권한 부여`)
-     } else {
-         console.log("잘못된 회원등급입니다.")
-     }
+//      if(grade === "1") {
+//          console.log(`${permission}권한 부여`)
+//      } else if (grade === "2") {
+//          console.log(`${permission}권한 부여\n${permission2}권한 부여`)
+//      } else if (grade === "3") {  
+//          console.log(`${permission}권한 부여 \n${permission2}권한 부여\n${permission3}권한 부여`)
+//      } else {
+//          console.log("잘못된 회원등급입니다.")
+//      }
+// }
+
+// function solution(score: number) {
+//     if(score >= 90 && score <= 100) {
+//         console.log("A")
+//     } else if(score >= 80 && score <= 89) {
+//         console.log("B")
+//     } else if(score >= 70 && score <= 79) {
+//         console.log("C")
+//     } else if(score >= 60 && score <= 69) {
+//         console.log("D")
+//     } else {
+//         console.log("F")
+//     }
+// }
+
+
+// 윤년 문제
+function solution(year: number) {
+    
 }
+
 
 export default solution;
