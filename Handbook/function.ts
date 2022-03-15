38feed39ba1088407df6ea7543062b674a62fac1
tomspacecloud <tom@spacecloud.kr>
2022-03-15 23:01:57 +0900

    TypeScript Function

--- Handbook/function.ts
+++ Handbook/function.ts
@@ -12,3 +12,45 @@ function builName(firstName: string, lastName: string) {
 // let result1 = builName("Bob"); // 오류, 너무 적은 매개변수
 // let result2 = builName("Bob", "Adams", "Sr."); // 오류, 너무 많은 매개변수
 // let result3 = builName("Bob", "Adams"); // 정확
+
+function buildName2(firstName: string, lastName?: string) {
+    if(lastName){
+        return firstName + " " + lastName;
+    } else 
+    return firstName
+}
+
+// let result1 = buildName2("Bob"); // 지금은 바르게 동작
+// let result2 = buildName2("Bob", "Adams", "Sr."); // 오류, 너무 많은 매개변수
+// let result3 = buildName2("Bob", "Adams"); // 정확함
+
+function buildName3(firstName: string, lastName = "Smith") {
+    return firstName + " " + lastName
+}
+
+// let result1 = buildName3("Bob") // 올바르게 동작
+// let result2 = buildName3("Bob", undefined); // 여전히 동작, "Bob Smith" 반환
+// let result3 = buildName3("Bob", "Adams", "Sr.") // 오류 너무 많은 매개변수
+// let result4 = buildName3("Bob", "Adams"); // 정확함
+
+function buildName4(firstName = "Will", lastName: string) {
+    return firstName + " " + lastName;
+}
+
+// let result1 = buildName4("Bob");                  // 오류, 너무 적은 매개변수
+// let result2 = buildName4("Bob", "Adams", "Sr.");  // 오류, 너무 많은 매개변수
+// let result3 = buildName4("Bob", "Adams");         // 성공, "Bob Adams" 반환
+// let result4 = buildName4(undefined, "Adams");     // 성공, "Will Adams" 반환
+
+function buildName5(firstName: string, ...restOfName: string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+// employeeName 은 "Joseph Samuel Lucas MacKinzie" 가 될것입니다.
+let employeeName = buildName5("Joseph", "Samuel", "Lucas", "MacKinzie");
+
+function buildName6(firstName: string, ...restOfName: string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let buildNameFun: (fname: string, ...rest: string[]) => string = buildName6;
