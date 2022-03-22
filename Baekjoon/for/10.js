eb1ed69fc5006fcacf654bccafa178f0b4834363
bellDev-code <sumaoo20@naver.com>
2022-03-22 19:42:43 +0900

    백준 for문 9번 완료, 10번 진행중

---
+++ Baekjoon/for/10.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync('10.txt').toString().split('\n');
+
+const [n, x] = input[0].split(' ');
+const test = input[1]
+
+
+
+console.log(n, x)
+console.log(test)
--- Baekjoon/for/9.js
+++ Baekjoon/for/9.js
@@ -9,7 +9,7 @@ let result = ''
 
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n - i - 1; j++) {
-      result += ',';
+      result += ' ';
     }
   
     for (let k = 0; k <= i; k++) {
