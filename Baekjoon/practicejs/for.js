e8fdfe862269a180063fddc36204d20b89e79fec
bellDev-code <sumaoo20@naver.com>
2022-04-05 22:25:29 +0900

    for문 정리

---
+++ Baekjoon/practicejs/for.js
@@ -0,0 +1,19 @@
+var i = 0
+for(; i < 9; i++) {
+	// console.log(i)
+	// 기타 등등
+}
+
+for (var i = 0;; i++) {
+    // console.log(i);
+    if (i > 3) break;
+    // 기타 등등
+ }
+
+ var i = 0;
+
+for (;;) {
+  if (i > 3) break;
+  console.log(i);
+  i++;
+}
