fd88fab35b6a3c57b29969a64a4c2d8838b9be98
bellDev-code <sumaoo20@naver.com>
2022-04-19 16:59:54 +0900

    백준 function 문제 푸는중

--- Baekjoon/array/7.js
+++ Baekjoon/array/7.js
@@ -5,23 +5,23 @@ let allCases = Number(input[0]);
 
 for (let i = 1; i <= allCases; i++) {
   let cases = input[i].split(" ");
-  let count = Number(cases[0]);
+  let value = Number(cases[0]);
   let sum = 0;
 
-  for (let j = 1; j <= count; j++) {
+  for (let j = 1; j <= value; j++) {
     sum += Number(cases[j]);
   }
 
-  let reverage = sum / count;
+  let reverage = sum / value;
 
   let highScore = 0;
 
-  for (let k = 1; k <= count; k++) {
+  for (let k = 1; k <= value; k++) {
     if (reverage < cases[k]) {
       highScore++;
     }
   }
 
-  result = ((highScore / count) * 100).toFixed(3);
+  result = ((highScore / value) * 100).toFixed(3);
   console.log(result + "%");
 }
---
+++ Baekjoon/function/2.js
@@ -0,0 +1,39 @@
+// 자기자신 + 각 자리의 숫자의 합으로 구분
+
+function notSelfNum(n) {
+  // 숫자 하나를 더하는 함수
+  // ex) 9645 넣으면 9645 + 5 + 4 + 6 + 9 = 9669
+  // n자리 숫자 1의 자리 더하고 1의 자리를 제거, 모든 자리수 다 더하자
+  let sum = n;
+
+  while (true) {
+    if (n === 0) {
+      break; // 1의 자리 수 제거, 결국 1의 자리 0
+    }
+    sum += n % 10; // 1의 자리 수 더하기
+    n = parseInt(n / 10);
+  }
+  return sum;
+}
+
+function selfPlusNum(n) {
+  let selfNum = [];
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    let index = notSelfNum(i); // index 변수에 notSelfNum() 함수를 넣는다.
+
+    if (index <= n) {
+      selfNum[index] = true;
+    }
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!selfNum[i]) {
+      result.push(i);
+    }
+    console.log(result.join("\n"));
+  }
+}
+
+selfPlusNum(100000);
