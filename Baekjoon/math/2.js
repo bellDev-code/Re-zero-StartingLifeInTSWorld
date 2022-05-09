19f616c378f58dd884e4a394bbdd64c30db2741d
bellDev-code <sumaoo20@naver.com>
2022-05-09 23:46:00 +0900

    백준 알고리즘

--- Baekjoon/math/2.js
+++ Baekjoon/math/2.js
@@ -2,3 +2,9 @@ const fs = require("fs");
 let input = fs.readFileSync("2.txt").toString().split(" ");
 
 console.log(input);
+
+let num = parseInt(input);
+
+// 2 3 4 5 6 7 -> 6, 2번
+// 8 9 10 11 12 13 14 15 16 17 18 19 -> 12, 3번
+// 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 38 -> 18, 4번
---
+++ Baekjoon/math/3.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let input = fs.readFileSync("3.txt").toString().split(" ");
+
+console.log(input);
+
+// 1 2 3
+// 1/1 1/2 2/1
